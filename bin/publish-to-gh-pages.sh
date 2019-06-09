8e519810102a77189e92cbe005a52c6d988d038b
dingziqi <dingziqi_xtu@sina.com>
2019-06-09 17:12:04 +0800

    提交漏提交文件

--- bin/publish-to-gh-pages.sh
+++ bin/publish-to-gh-pages.sh
@@ -6,7 +6,7 @@ git checkout master
 cd ../
 mv .deploy_git/.git/ ./public/
 cd ./public
-- git init
+git init
 git config user.name  "dingziqi"
 git config user.email "dingziqi_xtu@sina.com"
 # add commit timestamp
