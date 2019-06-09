cefe88b5561bf87654ab5ff1c6fb00d7fc330076
dingziqi <dingziqi_xtu@sina.com>
2019-06-09 17:38:18 +0800

    调整分支

--- bin/publish-to-gh-pages.sh
+++ bin/publish-to-gh-pages.sh
@@ -2,7 +2,7 @@
 set -ev
 git clone https://${GH_REF} .deploy_git
 cd .deploy_git
-git checkout master
+git checkout hexo
 cd ../
 mv .deploy_git/.git/ ./docs/
 cd ./docs
@@ -12,4 +12,4 @@ git config user.email "dingziqi_xtu@sina.com"
 # add commit timestamp
 git add .
 git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
-git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
+git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" hexo:master
