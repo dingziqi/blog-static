991f714be4b63ba032c2ac1be0ff08eadded2c13
dingziqi <dingziqi_xtu@sina.com>
2019-06-09 16:53:34 +0800

    增加CI

---
+++ bin/publish-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name  "dingziqi"
+git config user.email "dingziqi_xtu@sina.com"
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:gh
